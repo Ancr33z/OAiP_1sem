e7991a8dc7653ac3b55877c2c6f9a961faafed0e
Ancr33z <nikita.borisov20011@gmail.com>
2023-09-20 15:43:17 +0300

    .

---
+++ labaa6/labaa6-2/labaa6-2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream> 
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+
+	float b = -0.05, a = 1.72, i, d, z;
+
+	for (i = -5; i < 5; i += 2) {
+
+		if (i > 3*b) {
+
+			d = a+b*i;
+			z = (d * a / 4) / (3 * a * b - exp(1 + d) / 100);
+		}
+		else
+		{
+			d = tan(b)-a*i;
+			z = (d * a / 4) / (3 * a * b - exp(1 + d) / 100);
+		}
+		cout << "z = " << z << endl;
+		cout << "d = " << d << endl;
+
+	}
+
+	return 0;
+}
---
+++ labaa6/labaa6/labaa6-1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream> 
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+
+	float a = 1.774, m = 5, x,y,z;
+
+	for (x = 3; x < 5; x += 0.2) {
+
+		y = sqrt(a + pow(m, 2) * pow(x, 2))/(a+x)*m;
+
+		if (abs(y) < 1) {
+			z = y + 1;
+		}
+		else
+		{
+			z = pow(sin(y), 2);
+		}
+		cout << "z = " << z << endl;
+		cout << "y = " << y << endl;
+
+	}
+
+	return 0;
+}
