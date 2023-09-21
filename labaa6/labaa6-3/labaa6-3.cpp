9e4a42494747f1e5ae7840eaa7a8ed0c95885a9d
Ancr33z <nikita.borisov20011@gmail.com>
2023-09-21 19:55:04 +0300

    .

---
+++ labaa6/labaa6-3/labaa6-3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream> 
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+
+	int i,summ;
+	summ = 0;
+	for (i = 0; i < 200; i ++) {
+
+		if (i % 3 ==0) {
+			summ += i;
+		}
+	}
+	printf("%d", summ);
+	return 0;
+}
