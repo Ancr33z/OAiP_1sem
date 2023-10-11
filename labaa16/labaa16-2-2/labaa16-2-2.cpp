288dbc97b5610c8a48317186f3ae05bcec048412
Ancr33z <nikita.borisov20011@gmail.com>
2023-10-11 16:33:40 +0300

    .

--- labaa16/labaa16-2-2/labaa16-2-2.cpp
+++ labaa16/labaa16-2-2/labaa16-2-2.cpp
@@ -4,26 +4,52 @@ int Sum(const int args[], int count);
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    int count = 0, n;
-    printf("Введите количество аргументов");
+    int count = 0, n, result;
+    printf("Введите количество аргументов: ");
     scanf_s("%d", &n);
     int* values = (int*)malloc(0);
-    printf("Введите аргументы");
+    printf("Введите аргументы: ");
     do
     {
-        values= (int*)realloc(values, sizeof(int) * ++count);
+        values = (int*)realloc(values, sizeof(int) * ++count);
+        scanf_s("%d", &values[count - 1]);
+    } while (count < n);
+    count = 0;
+    result = Sum(values, n - 1);
+    printf("Сумма: %d\n", result);
+
+    printf("Введите количество аргументов: ");
+    scanf_s("%d", &n);
+    printf("Введите аргументы: ");
+    do
+    {
+        values = (int*)realloc(values, sizeof(int) * ++count);
+        scanf_s("%d", &values[count - 1]);
+    } while (count < n);
+    count = 0;
+
+    result = Sum(values, n - 1);
+    printf("Сумма: %d\n", result);
+
+    printf("Введите количество аргументов: ");
+    scanf_s("%d", &n);
+    printf("Введите аргументы: ");
+    do
+    {
+        values = (int*)realloc(values, sizeof(int) * ++count);
         scanf_s("%d", &values[count - 1]);
     } while (count < n);
 
-    int result = Sum(values, n-1);
-    printf("Сумма: %d", result);
+    result = Sum(values, n - 1);
+
+    printf("Сумма: %d\n", result);
     return 0;
 }
 
 int Sum(const int args[], int count) {
     int sum = 0;
     for (int i = 0; i < count; i++) {
-        sum += args[i]*args[i+1];
+        sum += args[i] * args[i + 1];
     }
     return sum;
 }
