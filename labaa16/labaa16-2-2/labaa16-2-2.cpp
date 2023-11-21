7cafb0aad587cb696b7ada59f5e83b2efa036ca0
Ancr33z <nikita.borisov20011@gmail.com>
2023-11-21 16:56:56 +0300

    .

--- labaa16/labaa16-2-2/labaa16-2-2.cpp
+++ labaa16/labaa16-2-2/labaa16-2-2.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 void main()
 {
-	cout << sum(6, 4, 5, 1, 2, 3, 0) << std::endl;
-	cout << sum(2, 34, 4445);
+	cout << sum(6, 4, 5, 1, 2, 3) << endl;
+	cout << sum(2, 34, 4445) << endl;
+	cout << sum(6, 4, 5, 1, 2, 5, 7, 5) << endl;
 }
 
 int sum(int n, ...)
 {
 	int* p = &n;
 	int sum = 0;	
-	for (int i = 1; i <= n; i++)
-		sum += *(++p);
-	return *p;
+	for (int i = 1; i < n; i++) {
+		sum += *(p+i) * *(p+i+1);
+	}
+	return sum;
 }
